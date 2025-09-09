c9d4a9d29506b6e86f0e0e16db20b3b3a6a5ad36
Arsen <arsenkr200777@gmail.com>
2025-09-09 10:56:44 +0300

    Added average calculation

---
+++ Lab_01/Task_03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Task03
+{
+    static void Main()
+    {
+        Console.WriteLine("1. Calculate average ");
+        
+        int choice = int.Parse(Console.ReadLine());
+        switch (choice) {
+            case 1:
+            float average;
+            Console.WriteLine();
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            average = (a + b + c) / 3;
+            Console.WriteLine(average);
+            break;
+        }
+        
+    }
+}
