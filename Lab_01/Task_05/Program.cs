66806ec916ad90d2c52bd064bf4576da5fc4b484
Arsen <arsenkr200777@gmail.com>
2025-09-10 17:02:05 +0300

    Compteled all lab

---
+++ Lab_01/Task_05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Task05
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter number: ");
+        int n =  int.Parse(Console.ReadLine());
+        int factorial = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            factorial *= i;
+        }
+        Console.WriteLine("Factorial: " + factorial);
+    }
+}
