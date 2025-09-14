68b9884b8c4c7f9e0ffd1aa20a3bfc8589b79404
Arsen <arsenkr200777@gmail.com>
2025-09-14 13:37:16 +0300

    Completed Task04

---
+++ Lab_02/Task_04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Task04
+{
+        static void Main()
+        {
+                int n = int.Parse(Console.ReadLine());
+                bool[] primes = new bool[n+1];
+
+                for (int i = 0; i <= n; i++)
+                {
+                        primes[i] = true;
+                }
+                primes[0] = false;
+                primes[1] = false;
+
+                for (int i = 2; i <= n; i++)
+                {
+                        if (primes[i])
+                        {
+                                Console.Write(i + " ");
+                                for (int j = i * 2; j <= n; j += i)
+                                {
+                                        primes[j] = false;
+                                }
+                        }
+                }
+        }
+}
