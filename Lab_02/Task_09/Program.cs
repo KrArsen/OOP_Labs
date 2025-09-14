c242bef0736f55bd7dd6a80877a3086344427ba7
Arsen <arsenkr200777@gmail.com>
2025-09-14 14:35:39 +0300

    Completed Task09

---
+++ Lab_02/Task_09/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Task09
+{
+    static void Main()
+    {
+        char[] alphabet = new char[26];
+        for (int i = 0; i < 26; i++)
+        {
+            alphabet[i] = (char)('a' + i);
+        }
+        string word = Console.ReadLine();
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            char ch = word[i];
+            int index = ch - 'a';
+            Console.WriteLine(ch + " -> " + index);
+        }
+    }
+}
