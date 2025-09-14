140d1ef824a6252bb09ea94ee6945bc54dad666a
Arsen <arsenkr200777@gmail.com>
2025-09-14 14:12:25 +0300

    Completed Task07

---
+++ Lab_02/Task_07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Task07
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] parts = input.Split(' ');
+        int [] arr = new int [parts.Length];
+        for (int i = 0; i < parts.Length; i++)
+        {
+            arr[i] = int.Parse(parts[i]);
+        }
+
+        int start = 0, len = 1;
+        int bestStart = 0, bestLen = 1;
+
+        for (int i = 1; i < parts.Length; i++)
+        {
+            if (arr[i] > arr[i - 1])
+            {
+                len++;
+            } else
+            {
+                len = 1;
+                start = i;
+            }
+
+            if (len > bestLen)
+            {
+                bestLen = len;
+                bestStart = i - len + 1;
+            }
+        }
+        for (int i = bestStart; i < bestStart + bestLen; i++)
+            {
+            Console.Write(arr[i] + " ");
+            }
+    }
+}
