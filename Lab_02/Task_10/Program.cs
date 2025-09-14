a817212d13fc2a5a517c8b497b9de71af83e95e5
Arsen <arsenkr200777@gmail.com>
2025-09-14 14:39:19 +0300

    Completed Task10

---
+++ Lab_02/Task_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Task10
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int[] numbers = new int[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            numbers[i] = int.Parse(input[i]);
+        }
+        int diff = int.Parse(Console.ReadLine());
+        int count = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = i + 1; j < numbers.Length; j++)
+            {
+                if (Math.Abs(numbers[i] - numbers[j]) == diff)
+                {
+                    count++;
+                }
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
