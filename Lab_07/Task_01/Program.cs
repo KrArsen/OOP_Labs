c03bc20f4ba3cbff3b834d8d1f070c464014d427
Arsen <arsenkr200777@gmail.com>
2025-10-08 20:09:50 +0300

    Task01 Completed

---
+++ Lab_07/Task_01/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+interface ICallable
+{
+    void Call(string number);
+}
+
+interface IBrowsable
+{
+    void Browse(string site); 
+}
+
+class Smartphone : ICallable, IBrowsable
+{
+    public void Call(string number)
+    {
+        
+        foreach (char c in number)
+        {
+            if (!char.IsDigit(c))
+            {
+                Console.WriteLine("Invalid number!");
+                return;
+            }
+        }
+
+        Console.WriteLine($"Calling... {number}");
+    }
+
+    public void Browse(string site)
+    {
+        
+        foreach (char c in site)
+        {
+            if (char.IsDigit(c))
+            {
+                Console.WriteLine("Invalid URL!");
+                return;
+            }
+        }
+
+        Console.WriteLine($"Browsing: {site}!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        string[] numbers = Console.ReadLine().Split(' ');
+        string[] sites = Console.ReadLine().Split(' ');;
+
+        Smartphone phone = new Smartphone();
+        
+        foreach (string number in numbers)
+        {
+            phone.Call(number);
+        }
+        
+        foreach (string site in sites)
+        {
+            phone.Browse(site);
+        }
+    }
+}
