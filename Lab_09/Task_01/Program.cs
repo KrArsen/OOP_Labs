81a75c6596a632b4ee50b73466bbc6414515e7fe
Arsen <arsenkr200777@gmail.com>
2025-10-15 21:33:51 +0300

    Task01 done

---
+++ Lab_09/Task_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Box<T>
+{
+    private T value;
+
+    public Box(T value)
+    {
+        this.value = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{value.GetType().FullName}: {value}";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        var intBox = new Box<int>(123123);
+        Console.WriteLine(intBox);
+
+        var stringBox = new Box<string>("life in a box");
+        Console.WriteLine(stringBox);
+    }
+}
