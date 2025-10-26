401dd52a4edc762d9e4a5852f2f9066fdacf825c
Arsen <arsenkr200777@gmail.com>
2025-10-26 20:01:08 +0200

    Task01

---
+++ Lab_11/Task_01/HarvestingFieldsTest.cs
@@ -0,0 +1,39 @@
+﻿namespace P01_HarvestingFields
+{
+    using System;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            Type classType = typeof(HarvestingFields);
+
+            FieldInfo[] fields = classType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            string command;
+            while ((command = Console.ReadLine()) != "HARVEST")
+            {
+                FieldInfo[] filteredFields = command switch
+                {
+                    "private" => fields.Where(f => f.IsPrivate).ToArray(),
+                    "protected" => fields.Where(f => f.IsFamily).ToArray(),
+                    "public" => fields.Where(f => f.IsPublic).ToArray(),
+                    "all" => fields,
+                    _ => Array.Empty<FieldInfo>()
+                };
+
+                foreach (var field in filteredFields)
+                {
+                    string accessModifier = field.IsPublic
+                        ? "public"
+                        : field.IsFamily
+                            ? "protected"
+                            : "private";
+
+                    Console.WriteLine($"{accessModifier} {field.FieldType.Name} {field.Name}");
+                }
+            }
+        }
+    }
+}
